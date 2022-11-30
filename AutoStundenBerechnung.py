8e9877aab3068e722e22105231f8d2f534781669
Kevin Thalmann <96835756+KevinThalmann222@users.noreply.github.com>
2022-11-30 13:50:26 +0100

    Updates

--- AutoStundenBerechnung.py
+++ AutoStundenBerechnung.py
@@ -1,22 +1,47 @@
 import openpyxl
+import os
 from pathlib import Path
 
 
+RESET = "\033[0m"
+BOLD = "\033[01m"
+UNDERLINE = "\033[04m"
+BLACK = "\033[30m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+ORANGE = "\033[33m"
+
+
 class AutoStundenBerechnung:
-    def __init__(self, wochenarbeitszeit_soll = 31) -> None:
+    def __init__(self, wochenarbeitszeit_soll=31.0) -> None:
         self.root_path = Path.cwd()
         self.wochenarbeitszeit_soll = wochenarbeitszeit_soll
+        os.system("color")
 
-    def get_excellist_path(self) -> list:
-        """returns the path of all excel sgvm files
+    def get_excellist_path(self) -> Path:
+        """_summary_
 
         Returns:
-            list: paths
+            Path: _description_
+
+        Yields:
+            Iterator[Path]: _description_
         """
         for xlsm_path in self.root_path.glob("*.xlsx"):
             yield xlsm_path
 
-    def lese_ist_wochenarbeitszeit(self, excel_path, excel_sheet = "Stundenaufstellung"):
+    def lese_ist_wochenarbeitszeit(
+        self, excel_path: Path, excel_sheet="Stundenaufstellung"
+    ) -> list:
+        """_summary_
+
+        Args:
+            excel_path (Path): _description_
+            excel_sheet (str, optional): _description_. Defaults to "Stundenaufstellung".
+
+        Returns:
+            list: _description_
+        """
         stundenaufstellung = []
         kalenderwoche = None
         wochenarbeitszeit = None
@@ -30,17 +55,52 @@ class AutoStundenBerechnung:
             for i_col in range(1, max_col):
                 cell_obj = wb[excel_sheet].cell(row=i_row, column=i_col)
                 if cell_obj.value == "Wochenarbeitszeit":
-                    wochenarbeitszeit = wb[excel_sheet].cell(row=i_row+1, column=i_col).value
-                    kalenderwoche = wb[excel_sheet].cell(row=i_row, column=i_col-1).value
+                    wochenarbeitszeit = (
+                        wb[excel_sheet].cell(row=i_row + 1, column=i_col).value
+                    )
+                    kalenderwoche = (
+                        wb[excel_sheet].cell(row=i_row, column=i_col - 1).value
+                    )
                 if kalenderwoche and wochenarbeitszeit:
-                    if (dic := {"kalenderwoche":kalenderwoche, "wochenarbeitszeit":wochenarbeitszeit}) not in stundenaufstellung:
+                    if (
+                        dic := {
+                            "kalenderwoche": kalenderwoche,
+                            "wochenarbeitszeit": wochenarbeitszeit,
+                        }
+                    ) not in stundenaufstellung:
                         stundenaufstellung.append(dic)
         return stundenaufstellung
 
     def berechne_wochenarbeitszeit(self):
+        """_summary_"""
+        ueberstunden_liste = []
         for excel_path in self.get_excellist_path():
             zeiten = self.lese_ist_wochenarbeitszeit(excel_path)
-            print(zeiten)
+            for zeit in zeiten:
+                ueberstunden = round(
+                    float(zeit["wochenarbeitszeit"])
+                    - float(self.wochenarbeitszeit_soll),
+                    2,
+                )
+                ueberstunden_liste.append(ueberstunden)
+                if ueberstunden > 0:
+                    print(
+                        f"{GREEN}Du hast in KW {zeit['kalenderwoche']} -> +{BOLD}{ueberstunden:.2f}h{RESET}{GREEN} zu VIEL gearbeitet.{RESET}"
+                    )
+                else:
+                    print(
+                        f"{RED}Du hast in KW {zeit['kalenderwoche']} -> {BOLD}{ueberstunden:.2f}h{RESET}{RED} zu WENIG arbeitet.{RESET}"
+                    )
+        total_ueberstunden = sum(ueberstunden_liste)
+        print("-" * 50)
+        if total_ueberstunden > 0:
+            print(
+                f"{GREEN}Deine gesamten Überstunde sind: {UNDERLINE}{BOLD}{total_ueberstunden:.2f}h.{RESET}"
+            )
+        else:
+            print(f"{RED}Du bist insgesamt mit {UNDERLINE}{BOLD}{total_ueberstunden:.2f}h{RESET}{RED} im minus.{RESET}")
+        print("-" * 50)
+
 
 if __name__ == "__main__":
     asb = AutoStundenBerechnung()
